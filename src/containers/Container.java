0925ecc2f00385cc4abd6ecf23a89e8b3b857b94
unknown <giuseppemetin1220@libero.it>
2021-01-12 18:14:27 +0100

    Exercise 30 of the Mooc java course with 3 classes

---
+++ src/containers/Container.java
@@ -0,0 +1,55 @@
+package containers;
+
+public class Container {
+    private double capacity;
+    private double volume;
+
+    public Container(double capacity) {
+        if (capacity <= 0) {
+            this.capacity = 0;
+        } else {
+            this.capacity = capacity;
+        }
+
+        this.volume = 0;
+    }
+
+    public double getVolume() {
+        return this.volume;
+    }
+
+    public double getOriginalCapacity() {
+        return this.capacity;
+    }
+
+    public double getCurrentCapacity() {
+        return this.capacity - this.volume;
+    }
+
+    public void addToTheContainer(double amount) {
+        if (amount < 0){
+            //Non fa nulla.
+        } else if((this.volume+amount)>this.capacity){
+            this.volume = this.capacity;
+        } else {
+            this.volume += amount;
+        }
+    }
+
+    public double takeFromTheContainer(double amount) {
+        if (amount < 0){
+            return 0;
+        } else if (amount > this.volume){
+            amount = this.volume;
+            this.volume = 0;
+            return amount;
+        } else {
+            this.volume -= amount;
+            return amount;
+        }
+    }
+
+    public String toString() {
+        return "volume = " + this.volume + ", free space " + getCurrentCapacity();
+    }
+}
---
+++ src/containers/ContainerHistory.java
@@ -0,0 +1,63 @@
+package containers;
+
+import java.util.ArrayList;
+
+public class ContainerHistory {
+    private ArrayList<Double> history;
+
+    public ContainerHistory() {
+        history = new ArrayList<>();
+    }
+
+    public void add(double situation) {
+        history.add(situation);
+    }
+
+    public void reset() {
+        history.clear();
+    }
+
+    public String toString() {
+        return history.toString();
+    }
+
+    public double maxValue() {
+        double maxValue = 0;
+
+        if (!history.isEmpty()) {
+            for (double value : history) {
+                if (value > maxValue) {
+                    maxValue = value;
+                }
+            }
+        }
+
+        return maxValue;
+    }
+
+    public double minValue() {
+        double minValue = 0;
+
+        if (!history.isEmpty()) {
+            for (double value : history) {
+                if (value > minValue) {
+                    minValue = value;
+                }
+            }
+        }
+
+        return minValue;
+    }
+
+    public double average() {
+        double average = 0;
+
+        if (!history.isEmpty()) {
+            for (double value : history) {
+                average += value;
+            }
+        }
+
+        return average /= history.size();
+    }
+}
